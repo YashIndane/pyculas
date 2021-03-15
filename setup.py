7cbf3cf0c756d5982c9811b26e1462cf725f95f8
Yash Indane <yashindane46@gmail.com>
2021-03-15 11:21:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md" , "r") as f:
 
 setuptools.setup(
       name="pyculas",
-      version="1.0.5",
+      version="1.0.6",
       author="Yash Indane",
       author_email="yashindane46@gmail.com",
       description="calculas python library",
